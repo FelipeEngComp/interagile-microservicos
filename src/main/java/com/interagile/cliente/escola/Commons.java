77d591d87ce843be7462f68c2a4827d6cd1af102
FelipeEngComp <feliperibeiroalves@gmail.com>
2020-02-20 20:57:27 -0300

    travis

---
+++ src/main/java/com/interagile/cliente/escola/Commons.java
@@ -0,0 +1,11 @@
+package com.interagile.cliente.escola;
+
+public class Commons {
+	
+	public static void main(String[] args) {
+		
+	}
+	
+
+}
+
---
+++ src/main/java/com/interagile/cliente/escola/model/CodigoMaterias.java
@@ -0,0 +1,13 @@
+package com.interagile.cliente.escola.model;
+
+import java.util.List;
+
+import lombok.Data;
+
+@Data
+public class CodigoMaterias {
+	
+	private List<String> codigos;
+
+	
+}
